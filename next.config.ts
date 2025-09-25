498eb92249f96e39d97f21d95c96c6aba65c306d
hdyoon <skymegod@gmail.com>
2025-09-25 14:49:00 +0900

    changed next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
       {
         source: "/api/:path*",
         destination: `${
-          process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080"
+          process.env.API_URL || "http://localhost:8080"
         }/api/:path*`,
       },
     ];
