b8b3ee7f71cde9408855da27a8914c1e155a6497
hdyoon <skymegod@gmail.com>
2025-09-25 18:20:40 +0900

    api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,13 +1,7 @@
 import axios from "axios";
 
 const getBaseURL = () => {
-  if (typeof window === "undefined") {
-    // 서버 사이드
-    console.log(process.env.API_URL);
-    return (process.env.API_URL || "http://localhost:8080") + "/api";
-  }
-  // 클라이언트 사이드
-  return process.env.NEXT_PUBLIC_API_URL + "/api";
+  return "/api";
 };
 
 const apiClient = axios.create({
