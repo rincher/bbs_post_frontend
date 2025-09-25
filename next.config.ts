bda0339b1d5aaefec10fbc7356298acab272d1d1
hdyoon <skymegod@gmail.com>
2025-09-25 13:39:54 +0900

    chanaged api route

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
       {
         source: "/api/:path*",
         destination: `${
-          process.env.API_URL || "http://localhost:8080"
+          process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080"
         }/api/:path*`,
       },
     ];
